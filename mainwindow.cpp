011a22761901dd688517492d713d480e5426ece2
Konstantin <kostochka1994@mail.ru>
2023-04-09 18:11:39 +0300

    Мелкие правки

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,7 +11,7 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    setWindowTitle("Задание");
+    setWindowTitle("Главное окно");
     QString programmPath = qApp->applicationDirPath();
 
     programmPath.append("/QtLanguage_en.qm");
